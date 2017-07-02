7ce2cf82b6163abcd0a602bda292f7fc1282e7e3
Michael Neumann <mneumann@ntecs.de>
2017-07-02 20:49:07 +0200

    Use bootstrap components build by build-1.17.0.sh.

--- build-1.18.0.sh
+++ build-1.18.0.sh
@@ -1,48 +1,51 @@
 #!/bin/sh
 
-BOOTSTRAP_VERSION=1.17.0
+RUSTC_BOOTSTRAP_VERSION=1.17.0
+CARGO_BOOTSTRAP_VERSION=0.18.0
 RUST_VERSION=1.18.0
+TARGET=x86_64-unknown-dragonfly
 
 BASE=`pwd`
-BOOTSTRAP_COMPILER_BASE=`realpath $BASE/bootstrap/rust-$BOOTSTRAP_VERSION-dev`
 DEST=$1
 DEST_INSTALL=$DEST/install
-ARG=$2
+COMPONENTS="cargo-${CARGO_BOOTSTRAP_VERSION} rust-std-${RUSTC_BOOTSTRAP_VERSION} rustc-${RUSTC_BOOTSTRAP_VERSION}"
 
-export LD_LIBRARY_PATH="${BOOTSTRAP_COMPILER_BASE}/lib:$LD_LIBRARY_PATH"
-#export RUST_LOG=debug
-export RUST_BACKTRACE=1
 
 # we need these to avoid patching the sources for sha256sum
 export PATH=$BASE/bin:$PATH
 
-if [ "$ARG" = "CONT" ]; then
-	cd $DEST
-	cd rustc-$RUST_VERSION-src
-else
-	rm -rf $DEST
-	mkdir $DEST
-	cd $DEST
-	#fetch https://static.rust-lang.org/dist/rustc-$RUST_VERSION-src.tar.gz
-	tar xvzf $BASE/dist/rustc-$RUST_VERSION-src.tar.gz
-	cd rustc-$RUST_VERSION-src
+rm -rf $DEST
+mkdir -p $DEST $DEST/tmp $DEST/bootstrap
 
-	for patch in $BASE/patches/patch-*; do
-		echo $patch
-		patch < $patch
-	done
+BOOTSTRAP_COMPILER_BASE=`realpath $DEST/bootstrap`
+export LD_LIBRARY_PATH="${BOOTSTRAP_COMPILER_BASE}/lib:$LD_LIBRARY_PATH"
 
-	./configure \
-		--enable-cargo-openssl-static --enable-extended \
-		--enable-local-rust --local-rust-root=${BOOTSTRAP_COMPILER_BASE} \
-		--llvm-root=/usr/local/llvm40 --prefix=$DEST_INSTALL
+for component in $COMPONENTS; do
+	tar xvzf $BASE/bootstrap/$component-${TARGET}.tar.gz -C $DEST/tmp
+	$DEST/tmp/$component-${TARGET}/install.sh --prefix=$DEST/bootstrap
+done
 
-	mkdir -p $DEST/rustc-$RUST_VERSION-src/build/x86_64-unknown-dragonfly/stage0
-	cpdup -vv $BOOTSTRAP_COMPILER_BASE $DEST/rustc-$RUST_VERSION-src/build/x86_64-unknown-dragonfly/stage0
+cd $DEST
 
-	# we need overrides for libz-sys to build cargo
-	mkdir -p $DEST/.cargo
-	echo "paths = [\"$BASE/patches/libz-sys\"]" >> $DEST/.cargo/config
-fi
+#fetch https://static.rust-lang.org/dist/rustc-$RUST_VERSION-src.tar.gz
+tar xvzf $BASE/dist/rustc-$RUST_VERSION-src.tar.gz
+cd rustc-$RUST_VERSION-src
+
+for patch in $BASE/patches/patch-*; do
+	echo $patch
+	patch < $patch
+done
+
+./configure \
+	--enable-cargo-openssl-static --enable-extended \
+	--enable-local-rust --local-rust-root=${BOOTSTRAP_COMPILER_BASE} \
+	--llvm-root=/usr/local/llvm40 --prefix=$DEST_INSTALL
+
+mkdir -p $DEST/rustc-$RUST_VERSION-src/build/x86_64-unknown-dragonfly/stage0
+cpdup -vv $BOOTSTRAP_COMPILER_BASE $DEST/rustc-$RUST_VERSION-src/build/x86_64-unknown-dragonfly/stage0
+
+# we need overrides for libz-sys to build cargo
+mkdir -p $DEST/.cargo
+echo "paths = [\"$BASE/patches/libz-sys\"]" >> $DEST/.cargo/config
 
 gmake VERBOSE=YES all dist install 2>&1 | tee out.log
